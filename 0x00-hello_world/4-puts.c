0b46e992652fba8ada3526e30bb4d48b82de0cfb
somoiretom <somoiretomcs@gmail.com>
2023-06-16 23:45:48 -0700

    C puts Function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main: prints "Programming is like building a multilingual puzzle
+ * Return: returns integer 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
