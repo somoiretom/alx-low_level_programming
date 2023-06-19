c73cffcd60926fe8f970900757433dc863eecd34
somoiretom <somoiretomcs@gmail.com>
2023-06-19 04:05:51 -0700

    Caps

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - main block of code
+ *
+ * Return: returns integer 0
+ */
+
+int main(void)
+{
+	char alphabets = 'a';
+	while (alphabets <= 'z')
+	{
+		putchar(alphabets);
+		alphabets++;
+	}
+
+	alphabets = 'A';
+	while (alphabets <= 'Z')
+	{
+		putchar(alpabets);
+		alphabets++;
+	}
+	putchar("\n");
+	return (0);
+}
+
