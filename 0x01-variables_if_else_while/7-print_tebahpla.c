52c325cd3e4c6a2e8b569c8d8132570443fe1d6e
somoiretom <somoiretomcs@gmail.com>
2023-06-19 12:28:02 -0700

    Lower case z-a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main code block
+ *
+ * Return: returns zero integer data type
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'z'; alphabet >= 'a'; alphabet--)
+
+	putchar(alphabet);
+	putchar('\n');
+
+	return (0);
+}
