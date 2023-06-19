ae60868c4ebe798b0fb971710133d1ef985abbb9
somoiretom <somoiretomcs@gmail.com>
2023-06-19 12:23:15 -0700

    Base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main code block
+ *
+ * Return: returns integer zero
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+
+	putchar(digit + '0');
+	putchar('\n');
+
+	return (0);
+}
