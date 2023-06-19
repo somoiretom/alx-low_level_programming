3ec597c43d5e6dbe3acd09f6d94bf483fb1c0ad0
somoiretom <somoiretomcs@gmail.com>
2023-06-19 12:17:59 -0700

    Single Digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main code block
+ *
+ * Return: returns integer zero
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+		printf("%i", digit);
+	putchar('\n');
+	return (0);
+}
