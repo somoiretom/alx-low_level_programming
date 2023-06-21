d0f2fb368fa385ae127db07ba0cd94738de8ac49
somoiretom <somoiretomcs@gmail.com>
2023-06-21 02:22:23 -0700

    Alphabets using _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - main code block
+ *
+ * Return: return int zero
+ */
+
+void print_alphabet(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar (a);
+		a++;
+
+	}
+	_putchar ('\n');
+	return (void);
+}
+
+	
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,5 +6,6 @@
  */
 
 int _putchar(char a);
+void print_alphabet(void);
 
 #endif
