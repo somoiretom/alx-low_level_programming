7a0f626c0a74331ad59d7e2f1bfebb998018e517
somoiretom <somoiretomcs@gmail.com>
2023-06-21 12:21:14 -0700

    Absolute value of n

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - returns an absolute value
+ * @n: int for an absolute value
+ *
+ * Return: returns int 0
+ */
+
+int _abs(int)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	return (n);
+}
