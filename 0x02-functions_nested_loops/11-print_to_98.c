5b8df02d270a4b34876546215821edf98888df1e
somoiretom <somoiretomcs@gmail.com>
2023-06-21 13:33:47 -0700

    print natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,7 +8,7 @@
 
 void print_to_98(int a)
 {
-int m, n;
+int m;
 if (a <= 98)
 {
 for (m= a; m <= 98; m++)
@@ -17,17 +17,6 @@ if (m != 98)
 printf("%d, ", m);
 else if (m == 98)
 	printf("%d\n", m);
-}
-}
-else if (a >= 98)
-{
-for (n = a; a >= 98; n--)
-{
-if (n != 98)
-printf("%d, ", n);
-else if (n == 98)
-printf("%d\n", n);
-}
 }
 }
 
