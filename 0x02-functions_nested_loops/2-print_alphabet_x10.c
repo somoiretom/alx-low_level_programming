ee06d416c7281264a78e30e7128da3697732e095
somoiretom <somoiretomcs@gmail.com>
2023-06-21 02:37:41 -0700

    Prints alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - code prototype for ALX projects
+ *
+ * Return: returns in o
+ */
+
+void print_alphabet_x10(void)
+{
+	int j = 0;
+	while(j < 10)
+	{char a = 'a';
+		while (a <= 'z')
+		{
+			_putchar(a);
+			a++;
+		}
+		_putchar('\n');
+		j++;
+	}
+}
