826156005e913f0a0cfe931b9037b76057ddbd87
somoiretom <somoiretomcs@gmail.com>
2023-06-22 00:15:32 -0700

    Positive or Negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - function where all codes a re executed
+ *
+ * Return: returns int 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
