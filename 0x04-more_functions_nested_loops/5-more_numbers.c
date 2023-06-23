cccf2e053d999f9662d229638c9b182ce8d5dc1a
somoiretom <somoiretomcs@gmail.com>
2023-06-23 03:38:36 -0700

    More numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 10 times the number
+ *
+ */
+void more_numbers(void)
+{
+	int m, n;
+
+	for (m = 0; m < 10; m++)
+	{
+		for (n = 0; n < 15; n++)
+		{
+			if (n >= 10)
+				_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
