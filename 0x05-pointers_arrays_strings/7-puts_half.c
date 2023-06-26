2a2ef8ee3f5254fcfb1d8f011b18747d40305833
somoiretom <somoiretomcs@gmail.com>
2023-06-26 11:11:16 -0700

    puts 2

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -24,7 +24,7 @@ void puts_half(char *str)
 		}
 	} else if (length % 2)
 	{
-		for (m = (length - 1) / 2; m < length - 1; n++)
+		for (n = (length - 1) / 2; n < length - 1; n++)
 		{
 			_putchar(str[n + 1]);
 		}
