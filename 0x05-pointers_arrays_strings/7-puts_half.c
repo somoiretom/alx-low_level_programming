bc4b2a701b1c862a756513d6f02bbb79a6d6bead
somoiretom <somoiretomcs@gmail.com>
2023-06-26 11:06:37 -0700

    puts 2

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * puts_half - prints a string, followed by a new line,
+ * @str: pointer to the string
+ * Return: returns 0
+ */
+
+void puts_half(char *str)
+{
+	int length, m, n;
+
+	length = 0;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	if (length % 2 == 0)
+	{
+		for (n = length / 2; str[n] != '\0'; n++)
+		{
+			_putchar(str[n]);
+		}
+	} else if (length % 2)
+	{
+		for (m = (length - 1) / 2; m < lenth - 1; n++)
+		{
+			_putchar(str[n + 1]);
+		}
+	}
+	_putchar('\n');
+}
