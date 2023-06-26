1108591aab365569df418133b8ae707f7d9a0a4a
somoiretom <somoiretomcs@gmail.com>
2023-06-26 03:29:45 -0700

    Print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _puts - outputs a string
+ * @str: pointer to print
+ * Return: return void
+ */
+
+
+void _puts(char *str)
+{
+	int x = 0;
+
+	while (str[x)
+	{
+		_putchar(str[x]);
+		x++;
+	}
+
+	_putchar('\n');
+}
