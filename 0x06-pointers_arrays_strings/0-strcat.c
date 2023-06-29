08de17da7ee64e16baac35b99113797f4a1fab29
somoiretom <somoiretomcs@gmail.com>
2023-06-29 11:31:45 -0700

    Concatenates

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings
+ * @dest: destination to a pointer
+ * @src: pointer source
+ * Return: returns dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = -1, a;
+	for (a = 0; dest[a] != '\0'; a++)
+	;
+
+	do {
+		i++;
+		dest[a] = src[i];
+		a++;
+	} while (src[i] != '\0');
+
+	return (dest);
+}
