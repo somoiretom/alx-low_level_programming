8b017904a6fbcc2cbc1ef5689a3ebc93dc43df3b
somoiretom <somoiretomcs@gmail.com>
2023-06-30 10:35:45 -0700

    Print number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - print int
+ * @n: integer
+ */
+void print_number(int n)
+{
+	int num1;
+
+	if (n < 0)
+	{
+		num1 = -n;
+		_putchar('-');
+	} else
+	{
+		num1 = n;
+	}
+
+	if (num1 / 10)
+		print_number(num1 / 10);
+
+	_putchar((num1 % 10) + '0');
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,4 +10,5 @@ void reverse_array(int *a, int n);
 int _strcmp(char *s1, char *s2);
 char *_strncpy(char *dest, char *src, int n);
 char *rot13(char *);
+void print_number(int n);
 #endif
