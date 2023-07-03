5d9e66b378e00f5be95b732673725e29f6c05d05
somoiretom <somoiretomcs@gmail.com>
2023-07-03 03:09:04 -0700

    Searching String

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strpbrk -  locates the first occurrence in the string s
+ * @s: String to search
+ * @accept: string 
+ *
+ * Return: pointer or NULL
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int x, y;
+
+	for (x = 0; *s != '\0'; x++)
+	{
+		for (y = 0; accept[y] != '\0'; y++)
+		{
+			if (*s == accept[y])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+
+	return (NULL);
+}
