8d7597dcf2815165f435bbdbe60863dd361ec4e0
somoiretom <somoiretomcs@gmail.com>
2023-07-08 14:09:49 -0700

    Static Libraries

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower - checks lowercase
+ * @c: character
+ * Return: 1 if c is lowercase, 0 elsewhere
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
